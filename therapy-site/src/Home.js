ed8b06391dc86d383f945affc559b13e248c79e8
Jay <jayermi61@gmail.com>
2025-01-15 18:45:07 -0600

    initial startup, few structual compnents

---
+++ therapy-site/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Affinity Counseling Associates</h1>
+      <p>Helping you find peace, one session at a time.</p>
+    </div>
+  );
+};
+
+export default Home;
